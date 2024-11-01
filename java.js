7d810828a902703b71f92355579750d6eaee5d7d
malekuvesh <116624435+malekuvesh@users.noreply.github.com>
2024-11-01 15:14:33 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,24 @@
+function update(cardId) {
+    // document.getElementById("younic").innerText = document.getElementById("ueszo").value;
+    document.getElementById("uveshes").innerText = document.getElementById("seates").value;
+    document.getElementById("younic").innerText = document.getElementById("ueszo").value;
+    // document.getElementById("scholes").innerText = document.getElementById("filedes").value;
+    document.getElementById("schol").innerText = document.getElementById("filed").value;
+
+    document.getElementById("name").innerText = document.getElementById("student").value;
+    document.getElementById("entry").innerText = document.getElementById("Date").value;
+    document.getElementById("uvesh").innerText = document.getElementById("seat").value;
+    document.getElementById("num").innerText = document.getElementById("roll").value;
+    document.getElementById("phone").innerText = document.getElementById("Contact").value;
+    document.getElementById("Add").innerText = document.getElementById("Address").value;
+    document.getElementById('card2').style.display = 'inline-block';
+    // document.getElementById('card1').style.display = 'none';
+
+}
+function printpages() {
+    // alert();
+    window.print();
+}
+function magic(){
+    alert("Your Data is Saved Successfully!");
+}
